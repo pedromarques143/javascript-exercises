f2eb89f8632457a0b8407a22dd68f7680af13f04
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-25 20:40:43 +0100

    Solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(sentence, reps) {
+    const senteceArr = [reps - 1];
+    if (reps == 0) {
+        return '';
+    } else if (reps < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < reps; i++) {
+            senteceArr[i] = sentence;
+        }
+    return senteceArr.join("");
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
