68e5f44e079627e6e4323fc05f91c27abe8a766f
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-25 21:06:57 +0100

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let sentenceLen = sentence.length;
+    const revertedSentence = [sentenceLen - 1];
+    if (sentenceLen == 0) {
+        return "";
+    } else {
+        for (let i = 0; i < sentence.length; i++) {
+            revertedSentence[i] = sentence[sentenceLen - 1 - i];
+        }
+    }
+    return revertedSentence.join("");
 };
 
 // Do not edit below this line
