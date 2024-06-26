d7b4f85fcfe5ad4f4287a76807cff837e11be8f8
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-26 20:31:17 +0100

    Added to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,36 @@
-const removeFromArray = function() {
-};
+
+const removeFromArray = function(userArray, ...toDelete) {
+    for (let delElement of toDelete) {
+        for (let element of userArray) {
+            if (userArray[element] == delElement) {
+                userArray.splice(element, 1);
+            }
+        }
+    }
+
+    for (let i = 0; 0 < userArray.length; i++) {
+        let tempOne = userArray[i - 1];
+        let tempTwo = userArray[i];
+
+        if (tempOne == tempTwo) {
+            userArray.splice(i, 1);
+            userArray.splice(i - 1);
+        }
+
+    }
+    return userArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//delItems = Array.from(toDelete);
+//for (let i = 0; i < userArray.length; i++) {
+//    for (let x = 0; x < delItems.length; x++) {       
+//        if (userArray.includes(delItems[x])) {
+//            userArray.splice(i, 1);
+//        }
+//    }
+//}
+//return userArray;
