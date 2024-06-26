6ceae274c3edde60972d2385b31db05499489870
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-26 23:26:37 +0100

    Solved first part of exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    if (numberOne < 0 || numberTwo < 0 ) {
+        return "ERROR";
+    } else if (typeof numberOne != "number" || typeof numberTwo != "number" ) {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
