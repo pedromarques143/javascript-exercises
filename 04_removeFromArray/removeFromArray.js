642198ba134164e70fc5f09bdf9f24b8b287bbda
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-26 23:18:19 +0100

    Solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,36 +1,16 @@
 
 const removeFromArray = function(userArray, ...toDelete) {
-    for (let delElement of toDelete) {
-        for (let element of userArray) {
-            if (userArray[element] == delElement) {
-                userArray.splice(element, 1);
-            }
+    const newArray = [];
+
+    for (let element of userArray) {
+        if (!toDelete.includes(element)) {
+            newArray.push(element);
         }
     }
 
-    for (let i = 0; 0 < userArray.length; i++) {
-        let tempOne = userArray[i - 1];
-        let tempTwo = userArray[i];
-
-        if (tempOne == tempTwo) {
-            userArray.splice(i, 1);
-            userArray.splice(i - 1);
-        }
-
-    }
-    return userArray;
+    return newArray;
 }
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
-
-//delItems = Array.from(toDelete);
-//for (let i = 0; i < userArray.length; i++) {
-//    for (let x = 0; x < delItems.length; x++) {       
-//        if (userArray.includes(delItems[x])) {
-//            userArray.splice(i, 1);
-//        }
-//    }
-//}
-//return userArray;
