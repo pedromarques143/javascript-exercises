307cf43855560acd12c2ff059d772c6af8756b55
pedromarques143 <pedromarques.trad@gmail.com>
2024-06-27 15:56:21 +0100

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,24 @@ const sumAll = function(numberOne, numberTwo) {
         return "ERROR";
     } else if (typeof numberOne != "number" || typeof numberTwo != "number" ) {
         return "ERROR";
+    } else {
+        const numberArr = [];
+        if (numberOne < numberTwo) {
+            for (let i = numberOne + 1; i < numberTwo; i++) {
+                numberArr.push(i);
+            }
+        } else {
+            for (let i = numberTwo + 1; i < numberOne; i++) {
+                numberArr.push(i);
+            }            
+        }
+        let totalSum = numberOne + numberTwo;
+
+        for (let i = 0; i < numberArr.length; i++) {
+            totalSum += numberArr[i];
+        }
+
+        return totalSum;
     }
 };
 
