d7af8c06d15cb9cb38e683267e0169264a1304c0
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-07 00:26:50 +0100

    Solved exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	const array = [];
+  for (let i = 1; i <= number; i++) {
+    array.push(i);
+  }
+  if (number === 0) {
+    return 1;
+  } else {
+    return array.reduce((total, current) => total * current);
+  }
 };
 
 // Do not edit below this line
