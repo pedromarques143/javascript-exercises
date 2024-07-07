df3020c0cd5a315755702bb364dbeb18e3188d0d
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-07 01:13:23 +0100

    Solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (userText) {
+    let textArr = [];
+    textArr = [...userText];
 
+    const simpleArr = textArr.filter(element => element != "!" && element != "." && element != "," && element != " ");
+    const invertedArr = simpleArr.toReversed();
+
+    let simpleStr = simpleArr.toString().toLowerCase();
+    let invertedStr = invertedArr.toString().toLowerCase();
+
+    if (simpleStr == invertedStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
