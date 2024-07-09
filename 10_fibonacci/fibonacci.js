adf28f76577ea9b0e8b1b4e4120853671e393194
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-09 17:49:41 +0100

    Solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const arr = [0, 1]
+    
+    if (number >= 0) {
+        for (let i = 2; i <= number; i++) {
+            arr[i] = Number(arr[i - 1] + arr[i - 2]);
+        }
 
+        return arr[number];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
