315ffbfba3b3b2ff13b4b5553df8aca0a56ea484
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-09 22:23:32 +0100

    Solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    
+    const newArray = peopleArray;
 
+    for (let person of newArray) {
+        if (person.yearOfDeath > 0) {
+            person.yearsLived = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.yearsLived = currentYear - person.yearOfBirth;
+        }
+    }
+
+    return oldestPerson = newArray.reduce((previous, current) => 
+        previous.yearsLived > current.yearsLived ? previous : current);
 };
 
 // Do not edit below this line
