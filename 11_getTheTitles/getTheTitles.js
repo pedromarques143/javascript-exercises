63079c7eb2222fc8ac3f22e32c3a6496486752a3
pedromarques143 <pedromarques.trad@gmail.com>
2024-07-09 17:58:08 +0100

    Solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titlesArr = [];
+    for (let book of books) {
+        titlesArr.push(book.title);
+    }
+    return titlesArr;
 };
 
 // Do not edit below this line
